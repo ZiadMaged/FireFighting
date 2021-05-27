d9d5d110403d001e283faac8eb9c8482cd22d578
unknown <Ziad_7@live.com>
2021-05-28 00:00:38 +0200

    change in package

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const app = express(); 
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/light-bootstrap-dashboard-angular2-master'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/light-bootstrap-dashboard-angular2-master/'}),
+    res.sendFile('index.html', {root: './dist/light-bootstrap-dashboard-angular2-master/'}),
 );
 
 // Start the app by listening on the default Heroku port
